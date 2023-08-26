7b3e079573e1a9d6de804a98b9318658c291e92b
이지환 <roovies77@gmail.com>
2023-08-26 19:33:36 +0900

    init: initial project

---
+++ src/main/java/com/roovies/securityjwtexample/SecurityJwtExampleApplication.java
@@ -0,0 +1,13 @@
+package com.roovies.securityjwtexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtExampleApplication.class, args);
+	}
+
+}
