2aefef6ba1d7bd815fca25687d8dd17ed1218ce9
roovies <roovies77@gmail.com>
2023-12-29 21:31:26 +0900

    feature: JWT 생성 및 검증 구현

---
+++ src/main/java/com/roovies/security/jwt/JwtProvider.java
@@ -0,0 +1,89 @@
+package com.roovies.security.jwt;
+
+import com.roovies.member.domain.entity.Authority;
+import com.roovies.security.service.JpaUserDetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+    @Value("${jwt.secret.key}")
+    private String salt;
+
+    private Key secretKey;
+
+    // 만료시간 : 1Hour
+    private final long exp = 1000L * 60 * 60;
+
+    private final JpaUserDetailsService userDetailsService;
+
+    @PostConstruct // WAS가 기동될 때 1번만 기본값을 세팅한 다음, 두고두고 쓸 수 있다.
+    protected void init() {
+        secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // 토큰 생성
+    public String createToken(String email, List<Authority> roles) {
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("roles", roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + exp))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 토큰에 담겨져있는 유저 email 획득
+    public String getEmail(String token) {
+        //createToken() 메소드를 보면, email 정보가 subject에 저장되고 있음
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // 권한 정보 획득
+    // Spring Security 인증 과정에서 권한 확인을 위한 기능
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getEmail(token)); // this는 JwtProvider 본인을 지칭
+        return new UsernamePasswordAuthenticationToken(token, "", userDetails.getAuthorities());
+    }
+
+    // Authorization Header를 통해 인증 수행
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            // Bearer 검증
+            if (!token.substring(0, "BEARER ".length()).equalsIgnoreCase("BEARER ")) {
+                return false;
+            } else {
+                token = token.split(" ")[1].trim();
+            }
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            // 만료되었을 시 false
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
