8ed6d146e22f351351b87129a6c1322052941049
roovies <roovies77@gmail.com>
2023-12-30 02:32:39 +0900

    fix: ddl-auto create 오류 해결
    기존에 ddl-auto를 했음에도 불구하고 어떠한 오류도 발생하지 않고 테이블이 자동으로 생성되지 않았다.

--- src/main/java/com/roovies/securityjwtexample/SecurityJwtExampleApplication.java
+++ src/main/java/com/roovies/securityjwtexample/SecurityJwtExampleApplication.java
@@ -2,8 +2,10 @@ package com.roovies.securityjwtexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.roovies")
 public class SecurityJwtExampleApplication {
 
 	public static void main(String[] args) {
