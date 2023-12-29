aca18e736e5289d55a8ec16e100a50237e0f2758
roovies <roovies77@gmail.com>
2023-12-29 21:43:52 +0900

    feature: 스프링 시큐리티 보안 정책 설정

---
+++ src/main/java/com/roovies/config/SecurityConfig.java
@@ -0,0 +1,102 @@
+package com.roovies.config;
+
+import com.roovies.security.jwt.JwtAuthenticationFilter;
+import com.roovies.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // ID, Password 문자열을 Base64로 인코딩하여 전달하는 구조
+                .httpBasic().disable()
+                // 쿠키 기반이 아닌 JWT 기반이므로 사용하지 않음
+                .csrf().disable()
+                // CORS 설정
+                .cors(c -> {
+                            CorsConfigurationSource source = request -> {
+                                // Cors 허용 패턴
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.setAllowedOrigins(List.of("*"));
+                                config.setAllowedMethods(List.of("*"));
+                                return config;
+                            };
+                            c.configurationSource(source);
+                        }
+                )
+                // 1. 스프링 시큐리티 세션 정책
+                // 1-1. 세션을 생성 및 사용하지 않음
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // 다음 정책 and()
+                .and()
+                // 2. 조건별로 요청 허용/제한 설정
+                .authorizeRequests()
+                // 2-1. 회원가입과 로그인 요청은 모두 승인
+                .antMatchers("/register", "login").permitAll()
+                // '/admin'으로 시작하는 요청은 ADMIN 권한이 있는 유저에게만 허용
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                // '/user'로 시작하는 요청은 USER 권한이 있는 유저에게만 허용
+                .antMatchers("/user/**").hasRole("USER")
+                // 이외의 모든 요청은 거부 (Default값은 모든 요청을 Deny하고, 위 인바운드 규칙으로 되어 있는 것만 허용하는 것)
+                .anyRequest().denyAll()
+                // 다음 정책
+                .and()
+                // JWT 인증 필터 적용
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                // 에러 핸들링
+                .exceptionHandling()
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        // 권한 문제가 발생할 경우 이 부분을 호출한다.
+                        response.setStatus(403);
+                        response.setCharacterEncoding("utf-8");
+                        response.setContentType("text/html; charset=UTF-8");
+                        response.getWriter().write("권한이 없는 사용자입니다.");
+                    }
+                })
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        // 인증 문제가 발생할 경우 이 부분을 호출한다.
+                        response.setStatus(401);
+                        response.setCharacterEncoding("utf-8");
+                        response.setContentType("text/html; charset=UTF-8");
+                        response.getWriter().write("인증되지 않은 사용자입니다.");
+                    }
+                })
+                ; // 정책 설정 종료
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
